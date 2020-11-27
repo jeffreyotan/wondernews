98fb1e9373742667342b0bb1f6731fd223c8d9b7
Jeffrey Tan <jeffreyotan@yahoo.com>
2020-11-27 16:53:16 +0800

    Changed routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,7 +15,7 @@ import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment'
 
 const ROUTES: Routes = [
-  { path: "", component: HomeComponent },
+  { path: "list", component: HomeComponent },
   { path: "home", component: HomeComponent },
   { path: "list", component: ListComponent },
   { path: "setting", component: SettingComponent },
